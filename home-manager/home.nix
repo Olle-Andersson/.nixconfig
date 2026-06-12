c89be9d346fe49532d6c747e96ef5c63bd044378
Olov Andersson <olov.andersson@pm.me>
2026-06-12 10:33:33 +0200

    nvim separated into its own file

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -11,21 +11,11 @@
     };
   };
 
-  home = {
-    username = "olle";
-    homeDirectory = "/home/olle";
-  };
-
-    programs.neovim = {
-  	enable = true;
-	sideloadInitLua = true;
+    home = {
+        username = "olle";
+        homeDirectory = "/home/olle";
     };
 
-  xdg.configFile."nvim" = {
-    recursive = true;
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nvim";
-  };
-
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
+    programs.home-manager.enable = true;
+    programs.git.enable = true;
 }
